07c531c33c2e28c522b0263751e3b24d145f7885
alandoyle <me@alandoyle.com>
2023-04-15 22:52:29 +0100

    Update solution to remove NuGet and add etra icons.

--- src/PowerTray/Build/Build.cs
+++ src/PowerTray/Build/Build.cs
@@ -1,5 +1,5 @@
 /******************************************************/
-/* Auto-generated [19:52:04 2023/04/15] - DO NOT EDIT */
+/* Auto-generated [22:48:01 2023/04/15] - DO NOT EDIT */
 /******************************************************/
 
 /*******************************************************************************************/
@@ -20,20 +20,20 @@ namespace PowerTray
 
 		public const ushort Major = 1;
 		public const ushort Minor = 1;
-		public const ushort Revision = 2;
+		public const ushort Revision = 3;
 		public const ushort Build = 297;
 		public const string Codename = "Heimdall";
 
-		public const string Full = "1.1.2.297";
-		public const string Short = "1.1.2";
+		public const string Full = "1.1.3.297";
+		public const string Short = "1.1.3";
 		public const string Tiny = "1.1";
 		public const string BuildInfo = "";
 
 		public const string StartYear = "2020";
 		public const string BuiltYear = "2023";
-		public const string BuildDate = "Apr 15 2023 19:52:04";
+		public const string BuildDate = "Apr 15 2023 22:48:01";
 		public const string BuildDateShort = "15/04/2023";
-		public const string BuildDateComp = "202304151952";
+		public const string BuildDateComp = "202304152248";
 		public const string BuiltBy = "aland";
 	}
 }
